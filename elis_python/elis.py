41161683a83c59baa9574c61a51c596ac6fb7091
Danil Borchevkin <danil.borchevkin@lab409.ru>
2016-03-20 20:17:09 +0200

    Added main python code

---
+++ elis_python/elis.py
@@ -0,0 +1,34 @@
+#-------------------------------------------------------------------------------
+# Name:        elis
+# Purpose:
+#
+# Author:      Danil Borchevkin
+#              Olga Borchevkina
+#
+# Created:     20.03.2016
+# Copyright:   (c) Danil&Olga Borchevkin 2016
+# Licence:     BSD 2-clause
+#-------------------------------------------------------------------------------
+
+
+
+def PlotGraph(firstChannel, secondChannel, metaData):
+
+    return None
+
+def GetFilename(dirPath):
+    filename = ''
+    return filename
+
+def ImportDataFromFile(filename):
+    firstChannel = []
+    secondChannel = []
+    metaData = {}
+
+    return firstChannel, secondChannel, metaData
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
